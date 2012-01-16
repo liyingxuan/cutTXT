c91780037754f7fb7a053583eb95748ee7d031d1
dracher <dracher@gmail.com>
2012-01-16 18:14:45 +0800

    nothing
    Signed-off-by: dracher <dracher@gmail.com>

--- SplitTxt.h
+++ SplitTxt.h
@@ -13,9 +13,9 @@ struct NameListStruct
 	char Name[100];
 }List[100];  //Global Strut Array
 
-void Show(void);//显示一些特定信息
-int ReadFileList(void);//读取exe文件所在目录的文件列表，并筛选出txt文档，放入全局的结构体数组中
-void PrintFileList(int row);//txt文档列表
-int ReScanf(int min,int max);//输入选择，错误后循环提示，直至输入正确
+void Show(void);
+int ReadFileList(void);
+void PrintFileList(int row);
+int ReScanf(int min,int max);
 
 #endif /* MAIN_H_ */
